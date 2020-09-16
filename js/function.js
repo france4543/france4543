8f7b1645a3fd727f458387040fa0d871e546053f
france4543 <50855675+france4543@users.noreply.github.com>
2020-09-16 14:13:48 +0700

    function description

---
+++ js/function.js
@@ -0,0 +1,23 @@
+
+
+
+function mul(a,b){
+    return a*b
+}
+
+function searchCustomer(customerName){
+    return{
+        name:"customer"
+    }
+}
+
+const addNew = (a, b) => {
+    this.name = " Wow This.name "
+    return a+b
+}
+const add = (a,b) =>{
+ return a + b
+}
+console.log(add(2,3))
+console.log(mul(2,3))
+console.log(searchCustomer())
