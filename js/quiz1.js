bf3170e770af788fea71c7e8d4f1dcc8dcd5ed02
france4543 <50855675+france4543@users.noreply.github.com>
2020-09-16 13:49:52 +0700

    add quiz1

---
+++ js/quiz1.js
@@ -0,0 +1,11 @@
+let profile = {
+    firstname: 'Wetpisit',
+    lastname: 'Poomirat',
+    age: 20,
+    nickname: 'France',
+    hobby: 'watching Movies',
+    tel: '0918780479'
+
+}
+
+console.log(profile)
