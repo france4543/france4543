5439f2ed8699ed3cac9c7039554936f16e2d81b2
france4543 <50855675+france4543@users.noreply.github.com>
2020-09-16 15:38:41 +0700

    create quiz4

---
+++ js/quiz4.js
@@ -0,0 +1,6 @@
+let word = 'Hello my name is Joke'
+console.log(word.length)
+
+const Joke = "Hello my name is Joke nice to meet you!"
+console.log(Joke.indexOf('Joke'))
+console.log("Well open up your mind and see like me".slice(5,9))
